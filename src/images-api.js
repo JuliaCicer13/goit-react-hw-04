e4309508ecbd56b3bb5eb329cf5a6850f4553b3f
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-03-30 02:30:53 +0200

    c

--- src/images-api.js
+++ src/images-api.js
@@ -4,12 +4,13 @@ axios.defaults.baseURL = "https://api.unsplash.com";
 
 const YOUR_ACCESS_KEY = 'FyesNUd9XuqGt5EtDgAuCNnctmFgVVbo05Q1yuAYgh4';
 
-export const fetchImagesWithTopic = async images => {
+export const fetchImagesWithTopic = async (query, page = 1) => {
   const response = await axios.get(`/search/photos`, {
     params: {
-     query: images,
+     query: query,
+     page: page,
      client_id: YOUR_ACCESS_KEY,
-     oer_page: 12,
+     per_page: 12,
     },
   });
   return response.data.results;
